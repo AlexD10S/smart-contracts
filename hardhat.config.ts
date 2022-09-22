611b100453a3290260e81ff3e0fb2915a9b60ab8
AlexD10S <alexfraga10@gmail.com>
2022-09-22 17:59:18 +0200

    added Frontier from Polkadot in the configration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    frontier: {
+      url:  "http://127.0.0.1:9933",
+      chainId: 42,
+      accounts: [
+        "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342",
+        "0xe2033d436ce0614acc1ee15bd20428b066013f827a15cc78b063f83ac0baae64",
+        "0xa17dd5704f87a9cd9bf78b552dc5ffc501399f64cab4cd724589d4d6fbb42db5",
+        "0x2e1d0fa418766f7a54421745631be952639ce5f68842a8ac80081cebfa64bdbf",
+      ],
+    },
     matic: {
       url:  process.env.ALCHEMY_URL,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
